3642f20ef3f42780baf7aad5ee43d22700e4a27a
Codexvn <24852067+Codexvn@users.noreply.github.com>
2019-07-01 21:26:45 +0800

    2019.07.01 09:26PM

---
+++ 2001.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double x1,x2,y1,y2;
+    cout<<fixed<<setprecision(2);
+    while (cin>>x1>>y1>>x2>>y2)
+        cout<<sqrt(pow(x2-x1,2)+pow(y2-y1,2))<<endl;
+
+}
