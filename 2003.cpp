76f0ab9c7bedf8321ecbe40d0e78cb2476777cb7
Codexvn <24852067+Codexvn@users.noreply.github.com>
2019-07-02 22:42:22 +0800

    2019.07.02 10:42PM

---
+++ 2003.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+	double get;
+	cout << fixed << setprecision(2);
+	unsigned long long* tmp;
+	while (cin >> get)
+	{
+		tmp= (unsigned long long*)&get;
+		*tmp = (*tmp << 1);
+		*tmp = (*tmp >> 1);
+		cout <<get<< endl;
+	}
+	return 0;
+}
