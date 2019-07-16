f5deaa6f6c5af736abd0ade85f5adb8602c59a6b
Codexvn <24852067+Codexvn@users.noreply.github.com>
2019-07-16 22:34:49 +0800

    2019.07.16 10:24PM

---
+++ 2012.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+bool prinum(int get);
+int main ()
+{
+    int x,y;
+    while (cin>>x>>y)
+    {
+        if (x!=0||y!=0)
+        {
+          for (int i = x; i <=y; i++)
+          {
+			  if (!prinum(i * i + i + 41))
+			  {
+				  cout << "Sorry" << endl;
+				  break;
+			  }
+              else if(i==y)
+                cout<<"OK"<<endl;           
+          }
+		}
+		else
+			break;
+    }
+}
+bool prinum(int get) //ÊÇ·ñÎªËØÊı
+{
+    for (int i = sqrt(get); i >1 ; i--)
+    {
+        if(!(get%i))
+        {
+            return false;
+            break;        
+        }
+    }
+    return true;
+}
